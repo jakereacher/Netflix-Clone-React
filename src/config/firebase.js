c4d66b1024cc54a6c70a984b917c0f48595e27a6
Rohan Jacob <123861149+jakereacher@users.noreply.github.com>
2025-09-08 18:30:24 +0530

    add

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-_hmuQ-XLZ_SAyk2UQku58gcLex1iiUk",
-  authDomain: "netflix-clone-react-6cad9.firebaseapp.com",
-  projectId: "netflix-clone-react-6cad9",
-  storageBucket: "netflix-clone-react-6cad9.firebasestorage.app",
-  messagingSenderId: "817699875370",
-  appId: "1:817699875370:web:afefbb0cfc887c032c7153"
+  apiKey: "AIzaSyA2OPDRUrxLKOaHr53uFAZXfJo5IalU37U",
+  authDomain: "netflix-clone-react-457af.firebaseapp.com",
+  projectId: "netflix-clone-react-457af",
+  storageBucket: "netflix-clone-react-457af.appspot.com",
+  messagingSenderId: "899434376748",
+  appId: "1:899434376748:web:a4cfe98a22a02953eaab89",
+  measurementId: "G-FXQWTTVR59"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export default app;
